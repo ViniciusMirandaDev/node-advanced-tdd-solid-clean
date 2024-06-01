65076fc3537fb594766c3dbe337f9ac167848bde
ViniciusMirandaDev <vinimb07@gmail.com>
2024-06-01 14:58:46 -0300

    feat: ensure PgUserAccountRepository returns account id

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -1,5 +1,5 @@
-import { PgUser } from '@/infra/postgres/entities'
 import { PgUserAccountRepository } from '@/infra/postgres/repos'
+import { PgUser } from '@/infra/postgres/entities'
 import { makeFakeDb } from '@/tests/infra/postgres/mocks'
 
 import { IBackup } from 'pg-mem'
@@ -27,47 +27,46 @@ describe('PgUserAccountRepository', () => {
 
   describe('load', () => {
     it('should return an account if email exists', async () => {
-      await pgUserRepo.save({ email: 'existing_email' })
+      await pgUserRepo.save({ email: 'any_email' })
 
-      const account = await sut.load({ email: 'existing_email' })
+      const account = await sut.load({ email: 'any_email' })
 
       expect(account).toEqual({ id: '1' })
     })
 
     it('should return undefined if email does not exists', async () => {
-      const account = await sut.load({ email: 'new_email' })
+      const account = await sut.load({ email: 'any_email' })
 
-      expect(account).toBe(undefined)
+      expect(account).toBeUndefined()
     })
   })
 
   describe('saveWithFacebook', () => {
     it('should create an account if id is undefined', async () => {
-      await sut.saveWithFacebook({
+      const { id } = await sut.saveWithFacebook({
         email: 'any_email',
         name: 'any_name',
         facebookId: 'any_fb_id'
       })
-
       const pgUser = await pgUserRepo.findOne({ email: 'any_email' })
 
       expect(pgUser?.id).toBe(1)
+      expect(id).toBe('1')
     })
 
-    it('should update an account if id is defined', async () => {
+    it('should update account if id is defined', async () => {
       await pgUserRepo.save({
         email: 'any_email',
         name: 'any_name',
         facebookId: 'any_fb_id'
       })
 
-      await sut.saveWithFacebook({
+      const { id } = await sut.saveWithFacebook({
         id: '1',
         email: 'new_email',
         name: 'new_name',
         facebookId: 'new_fb_id'
       })
-
       const pgUser = await pgUserRepo.findOne({ id: 1 })
 
       expect(pgUser).toEqual({
@@ -76,6 +75,7 @@ describe('PgUserAccountRepository', () => {
         name: 'new_name',
         facebookId: 'new_fb_id'
       })
+      expect(id).toBe('1')
     })
   })
 })
