9026c6cd963b4e882f477a987444f6abfeb0397d
ViniciusBaptistaCertacon <vinicius.baptista@certacon.com.br>
2024-06-01 00:02:35 -0300

    feat: ensure FacebookApi return FacebookUser

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -1,7 +1,7 @@
 import { HttpGetClient } from '@/infra/http'
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
 
-export class FacebookApi {
+export class FacebookApi implements LoadFacebookUserApi {
   private readonly baseUrl = 'https://graph.facebook.com'
 
   constructor (
@@ -10,7 +10,7 @@ export class FacebookApi {
     private readonly clientSecret: string
   ) {}
 
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
     const appToken = await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
@@ -19,7 +19,6 @@ export class FacebookApi {
         grant_type: 'client_credentials'
       }
     })
-
     const debugToken = await this.httpClient.get({
       url: `${this.baseUrl}/debug_token`,
       params: {
@@ -27,8 +26,7 @@ export class FacebookApi {
         input_token: params.token
       }
     })
-
-    await this.httpClient.get({
+    const userInfo = await this.httpClient.get({
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       url: `${this.baseUrl}/${debugToken.data.user_id}`,
       params: {
@@ -36,5 +34,10 @@ export class FacebookApi {
         access_token: params.token
       }
     })
+    return {
+      facebookId: userInfo.id,
+      name: userInfo.name,
+      email: userInfo.email
+    }
   }
 }
