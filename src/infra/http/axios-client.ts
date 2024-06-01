ab7b4ce6fd1c5dd87743ac4c7a7dcbc72ebdd4eb
ViniciusBaptistaCertacon <vinicius.baptista@certacon.com.br>
2024-06-01 11:46:33 -0300

    refactor: move AxiosHttpClient to each own file

---
+++ src/infra/http/axios-client.ts
@@ -0,0 +1,10 @@
+import { HttpGetClient } from '@/infra/http'
+
+import axios from 'axios'
+
+export class AxiosHttpClient {
+  async get <T = any> (params: HttpGetClient.Params): Promise<T> {
+    const result = await axios.get(params.url, { params: params.params })
+    return result.data
+  }
+}
