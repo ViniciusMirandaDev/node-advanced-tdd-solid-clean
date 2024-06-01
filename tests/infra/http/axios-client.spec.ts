370b1e3a49bfb4435d51cd884c71234920f378d5
ViniciusBaptistaCertacon <vinicius.baptista@certacon.com.br>
2024-06-01 11:41:56 -0300

    feat: ensure AxiosHttpClient return data on success

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -5,10 +5,9 @@ import axios from 'axios'
 jest.mock('axios')
 
 class AxiosHttpClient {
-  async get (params: HttpGetClient.Params): Promise<void> {
-    await axios.get(params.url, {
-      params: params.params
-    })
+  async get <T = any> (params: HttpGetClient.Params): Promise<T> {
+    const result = await axios.get(params.url, { params: params.params })
+    return result.data
   }
 }
 
@@ -22,6 +21,10 @@ describe('AxiosHttpClient', () => {
     fakeAxios = axios as jest.Mocked< typeof axios>
     url = 'any_url'
     params = { any: 'any' }
+    fakeAxios.get.mockResolvedValue({
+      status: 200,
+      data: 'any_data'
+    })
   })
 
   beforeEach(() => {
@@ -34,6 +37,12 @@ describe('AxiosHttpClient', () => {
 
       expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
+    })
+
+    it('should return data on success', async () => {
+      const result = await sut.get({ url, params })
+
+      expect(result).toEqual('any_data')
     })
   })
 })
