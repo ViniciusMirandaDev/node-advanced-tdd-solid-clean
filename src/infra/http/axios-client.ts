dfdf07d4d71e35eac3e9a91c81e8be14f0c3d013
ViniciusMirandaDev <vinimb07@gmail.com>
2024-06-01 13:22:50 -0300

    feat: add interface implementation

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -2,7 +2,7 @@ import { HttpGetClient } from '@/infra/http'
 
 import axios from 'axios'
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements HttpGetClient {
   async get <T = any> (params: HttpGetClient.Params): Promise<T> {
     const result = await axios.get(params.url, { params: params.params })
     return result.data
