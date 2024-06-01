e79298c111a763742c9e1e810a011227afe60ddc
ViniciusMirandaDev <vinimb07@gmail.com>
2024-06-01 14:13:13 -0300

    refactor: move production code to their own files

---
+++ src/infra/postgres/entities/user.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity({ name: 'usuarios' })
+export class PgUser {
+  @PrimaryGeneratedColumn()
+  id!: number
+
+  @Column({ name: 'nome', nullable: true })
+  name?: string
+
+  @Column()
+  email!: string
+
+  @Column({ name: 'id_facebook', nullable: true })
+  facebookId?: string
+}
