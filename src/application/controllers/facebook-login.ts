c4206e090a0d50ef8a5cc785e9e6be3b5aff5c5f
ViniciusMirandaDev <vinimb07@gmail.com>
2024-06-02 10:46:29 -0300

    feat: use generics instead of any

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,17 +1,39 @@
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
-import { HttpResponse, badRequest, ok, serverError, unauthorized } from '@/application/helpers'
+import {
+  HttpResponse,
+  badRequest,
+  ok,
+  serverError,
+  unauthorized
+} from '@/application/helpers'
 import { RequiredFieldError } from '@/application/errors'
 
-export class FacebookLoginController {
-  constructor (private readonly facebookAuthentication: FacebookAuthentication) {}
+type HttpRequest = {
+  token: string | undefined | null
+}
 
-  async handle (httpRequest: any): Promise<HttpResponse> {
+type Model = Error | {
+  accessToken: string
+}
+
+export class FacebookLoginController {
+  constructor (
+    private readonly facebookAuthentication: FacebookAuthentication
+  ) {}
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
-      if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
+      if (
+        httpRequest.token === '' ||
+        httpRequest.token === null ||
+        httpRequest.token === undefined
+      ) {
         return badRequest(new RequiredFieldError('token'))
       }
-      const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
+      const accessToken = await this.facebookAuthentication.perform({
+        token: httpRequest.token
+      })
       if (accessToken instanceof AccessToken) {
         return ok({
           accessToken: accessToken.value
